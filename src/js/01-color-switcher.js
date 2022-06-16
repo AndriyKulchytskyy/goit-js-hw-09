cdb0c026a54ba3d75d987858b519a1495520b061
Andriy Kulchytskyy <ak250578@gmail.com>
2022-06-16 17:48:53 +0200

    task 1
    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+let intID = null;
+
+refs.start.addEventListener('click', onStart);
+
+function onStart() {
+  intID = setInterval(changeBgCol, 1000);
+  refs.start.classList.add('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgCol() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+refs.stop.addEventListener('click', onStop);
+
+function onStop() {
+  clearInterval(intID);
+  refs.start.classList.remove('disabled');
+}
